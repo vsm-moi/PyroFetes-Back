c128cd453d7c9ba7623948951ee49d7efcb049f4
sanchezvem <mathys.sanchezvende@gmail.com>
2025-10-03 16:57:23 +0100

    Add F4T2NumberApproval and F4T2ExpirationDate in Staff.css

--- PyroFetes/Models/Staff.cs
+++ PyroFetes/Models/Staff.cs
@@ -9,5 +9,7 @@ public class Staff
     [Required, MaxLength(60)] public string LastName { get; set; } = null!;
     [Required, MaxLength(100)] public string? Profession { get; set; }
     [Required, MaxLength(120)] public string? Email { get; set; }
+    [Required] public string? F4T2NumberApproval { get; set; }
+    [Required] public DateOnly F4T2ExpirationDate { get; set; }
     public ICollection<Show>? Shows { get; set; }
 }
