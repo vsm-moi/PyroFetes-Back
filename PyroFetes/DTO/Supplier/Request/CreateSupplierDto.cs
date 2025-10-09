a262fb094c30ac974b345a846b7166bbf3c00407
sanchezvem <mathys.sanchezvende@gmail.com>
2025-10-09 17:47:12 +0200

    Creating all supplier dto

---
+++ PyroFetes/DTO/Supplier/Request/CreateSupplierDto.cs
@@ -0,0 +1,12 @@
+namespace PyroFetes.DTO.Supplier.Request;
+
+public class CreateSupplierDto
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string? Address { get; set; }
+    public int ZipCode { get; set; }
+    public string? City { get; set; }
+    public int DeliveryDelay { get; set; }
+}
---
+++ PyroFetes/DTO/Supplier/Request/PatchSupplierDeliveryDelayDto.cs
@@ -0,0 +1,7 @@
+namespace PyroFetes.DTO.Supplier.Request;
+
+public class PatchSupplierDeliveryDelayDto
+{
+    public int Id { get; set; }
+    public int DeliveryDelay { get; set; }
+}
