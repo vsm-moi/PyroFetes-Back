15526d15893300eba1f0ddd859d164ce646843b8
sanchezvem <mathys.sanchezvende@gmail.com>
2025-10-09 19:04:22 +0100

    delete productsellingprice

--- PyroFetes/DTO/QuotationProduct/Request/CreateQuotationProductDto.cs
+++ PyroFetes/DTO/QuotationProduct/Request/CreateQuotationProductDto.cs
@@ -16,7 +16,6 @@ public class CreateQuotationProductDto
     public int ProductApprovalNumber { get; set; }
     public decimal ProductWeight { get; set; }
     public decimal ProductNec { get; set; }
-    public decimal ProductSellingPrice { get; set; }
     public string? ProductImage { get; set; }
     public string? ProductLink { get; set; }
     public int ProductMinimalQuantity { get; set; }
--- PyroFetes/DTO/QuotationProduct/Response/GetQuotationProductDto.cs
+++ PyroFetes/DTO/QuotationProduct/Response/GetQuotationProductDto.cs
@@ -17,7 +17,6 @@ public class GetQuotationProductDto
     public int ProductApprovalNumber { get; set; }
     public decimal ProductWeight { get; set; }
     public decimal ProductNec { get; set; }
-    public decimal ProductSellingPrice { get; set; }
     public string? ProductImage { get; set; }
     public string? ProductLink { get; set; }
     public int ProductMinimalQuantity { get; set; }
