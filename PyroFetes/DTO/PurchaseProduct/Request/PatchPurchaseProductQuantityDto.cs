64fd223a6339e6d9b89cda4d37e0a2c287e0df7a
sanchezvem <mathys.sanchezvende@gmail.com>
2025-10-09 17:09:33 +0200

    Fix an error in PatchPurchaseProductQuantityDto.cs

--- PyroFetes/DTO/PurchaseProduct/Request/PatchPurchaseProductQuantityDto.cs
+++ PyroFetes/DTO/PurchaseProduct/Request/PatchPurchaseProductQuantityDto.cs
@@ -4,8 +4,4 @@ public class PatchPurchaseProductQuantityDto
 {
     public int Id { get; set; }
     public int Quantity { get; set; } 
-    
-    public int ProductId { get; set; }
-    
-    public int PurchaseOrderId { get; set; }
 }
