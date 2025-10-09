25d1407bd22292a3074a7427334c42f1720e1571
sanchezvem <mathys.sanchezvende@gmail.com>
2025-10-09 17:54:28 +0200

    Creating of all product dto

---
+++ PyroFetes/DTO/Product/Response/GetProductDto.cs
@@ -0,0 +1,17 @@
+namespace PyroFetes.DTO.Product.Response;
+
+public class GetProductDto
+{
+    public int Id { get; set; }
+    public int References { get; set; }
+    public string? Name { get; set; }
+    public decimal Duration {get; set;} 
+    public decimal Caliber { get; set; }
+    public int ApprovalNumber { get; set; }
+    public decimal Weight { get; set; }
+    public decimal Nec { get; set; }
+    public decimal SellingPrice { get; set; }
+    public string? Image { get; set; }
+    public string? Link { get; set; }
+    public int MinimalQuantity { get; set; }
+}
