8371423b16287b33781739f99bff76c89a818f20
sanchezvem <mathys.sanchezvende@gmail.com>
2025-10-10 10:35:23 +0200

    Editing Product.cs

--- PyroFetes/Models/Product.cs
+++ PyroFetes/Models/Product.cs
@@ -5,14 +5,13 @@ namespace PyroFetes.Models
     public class Product
     {
         [Key] public int Id { get; set; }
-        [Required] public int References { get; set; }
+        [Required, MaxLength(20)] public string? Reference { get; set; }
         [Required, MaxLength(100)] public string? Name { get; set; }
         [Required] public decimal Duration {get; set;} 
         [Required] public decimal Caliber { get; set; }
         [Required] public int ApprovalNumber { get; set; }
         [Required] public decimal Weight { get; set; }
         [Required] public decimal Nec { get; set; }
-        [Required] public decimal SellingPrice { get; set; }
         [Required] public string? Image { get; set; }
         [Required, MaxLength(200)] public string? Link { get; set; }
         [Required] public int MinimalQuantity { get; set; }
