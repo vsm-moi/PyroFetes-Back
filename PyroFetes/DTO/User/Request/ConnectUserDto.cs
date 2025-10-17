06c64a9f3fe7d4c23aa318ad9f7c43b0e6936c25
sanchezvem <mathys.sanchezvende@gmail.com>
2025-10-17 16:53:00 +0100

    add token

---
+++ PyroFetes/DTO/User/Request/ConnectUserDto.cs
@@ -0,0 +1,7 @@
+﻿namespace PyroFetes.DTO.User.Request;
+
+public class ConnectUserDto
+{
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+}
---
+++ PyroFetes/DTO/User/Response/GetTokenDto.cs
@@ -0,0 +1,6 @@
+﻿namespace PyroFetes.DTO.User.Response;
+
+public class GetTokenDto
+{
+    public string? Token { get; set; }
+}
