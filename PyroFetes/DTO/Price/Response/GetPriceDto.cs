a535f8cfebc3e598ffd6978b9c6c032727b70774
sanchezvem <mathys.sanchezvende@gmail.com>
2025-11-06 18:51:46 +0100

    fix errors in Price's DTO

--- PyroFetes/DTO/Price/Response/GetPriceDto.cs
+++ PyroFetes/DTO/Price/Response/GetPriceDto.cs
@@ -15,7 +15,7 @@ public class GetPriceDto
     public int SupplierDeliveryDelay { get; set; }
     
     public int ProductId { get; set; }
-    public int ProductReferences { get; set; }
+    public string? ProductReferences { get; set; }
     public string? ProductName { get; set; }
     public decimal ProductDuration {get; set;} 
     public decimal ProductCaliber { get; set; }
