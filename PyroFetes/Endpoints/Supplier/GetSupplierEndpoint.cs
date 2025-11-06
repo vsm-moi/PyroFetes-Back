be0a6a40042a0a70883989c7ae85d367fa5c22d6
sanchezvem <mathys.sanchezvende@gmail.com>
2025-11-06 17:30:03 +0100

    Creating supplier's endpoints

---
+++ PyroFetes/Endpoints/Supplier/GetSupplierEndpoint.cs
@@ -0,0 +1,43 @@
+﻿using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using PyroFetes.DTO.Supplier.Response;
+
+namespace PyroFetes.Endpoints.Supplier;
+
+public class GetSupplierRequest
+{
+    public int Id { get; set; }
+}
+
+public class GetSupplierEndpoint(PyroFetesDbContext database) : Endpoint<GetSupplierRequest, GetSupplierDto>
+{
+    public override void Configure()
+    {
+        Get("/api/suppliers/{@Id}", x => new {x.Id});
+    }
+    
+    public override async Task HandleAsync(GetSupplierRequest req, CancellationToken ct)
+    {
+        var supplier = await database.Suppliers
+            .SingleOrDefaultAsync(x => x.Id == req.Id, ct);
+
+        if (supplier == null)
+        {
+            await Send.NotFoundAsync(ct);
+            return;
+        }
+
+        GetSupplierDto responseDto = new()
+        {
+            Id = supplier.Id,
+            Name = supplier.Name,
+            Email = supplier.Email,
+            Phone = supplier.Phone,
+            Address = supplier.Address,
+            City = supplier.City,
+            ZipCode = supplier.ZipCode,
+            DeliveryDelay = supplier.DeliveryDelay
+        };
+        await Send.OkAsync(responseDto, ct);
+    }
+}
---
+++ PyroFetes/Endpoints/Supplier/UpdateSupplierEndpoint.cs
@@ -0,0 +1,48 @@
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using PyroFetes.DTO.Supplier.Request;
+using PyroFetes.DTO.Supplier.Response;
+
+namespace PyroFetes.Endpoints.Supplier;
+
+public class UpdateSupplierEndpoint(PyroFetesDbContext database) : Endpoint<UpdateSupplierDto, GetSupplierDto>
+{
+    public override void Configure()
+    {
+        Put("/api/suppliers/{@Id}", x => new {x.Id});
+    }
+
+    public override async Task HandleAsync(UpdateSupplierDto req, CancellationToken ct)
+    {
+        var supplier = await database.Suppliers.SingleOrDefaultAsync(x => x.Id == req.Id, ct);
+        
+        if (supplier == null)
+        {
+            await Send.NotFoundAsync(ct);
+            return;
+        }
+
+        supplier.Name = req.Name;
+        supplier.Email = req.Email;
+        supplier.Phone = req.Phone;
+        supplier.Address = req.Address;
+        supplier.City = req.City;
+        supplier.ZipCode = req.ZipCode;
+        supplier.DeliveryDelay = req.DeliveryDelay;
+        await database.SaveChangesAsync(ct);
+        
+        GetSupplierDto responseDto = new()
+        {
+            Id = supplier.Id,
+            Name = supplier.Name,
+            Email = supplier.Email,
+            Phone = supplier.Phone,
+            Address = supplier.Address,
+            City = supplier.City,
+            ZipCode = supplier.ZipCode,
+            DeliveryDelay = supplier.DeliveryDelay
+        };
+        
+        await Send.OkAsync(responseDto, ct);
+    }
+}
