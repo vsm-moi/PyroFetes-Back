efa7a0be6f3c84988046f6a276ab425717c56575
sanchezvem <mathys.sanchezvende@gmail.com>
2025-11-29 21:58:20 +0100

    added DeleteProductEndpoint.cs

---
+++ PyroFetes/Endpoints/Products/DeleteProductEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using FastEndpoints;
+using PyroFetes.Endpoints.Deliverers;
+using PyroFetes.Models;
+using PyroFetes.Repositories;
+using PyroFetes.Specifications.Deliverers;
+using PyroFetes.Specifications.Products;
+
+namespace PyroFetes.Endpoints.Products;
+
+public class DeleteProductsRequest
+{
+    public int ProductId { get; set; }
+}
+public class DeleteProductEndpoint(ProductsRepository productsRepository) : Endpoint<DeleteProductsRequest>
+{
+    public override void Configure()
+    {
+        Delete("/products/{@id}", x=>new {x.ProductId});
+        AllowAnonymous();
+
+    }
+
+    public override async Task HandleAsync(DeleteProductsRequest req, CancellationToken ct)
+    {
+        Product? product = await productsRepository.FirstOrDefaultAsync(new GetProductByIdSpec(req.ProductId), ct);
+
+        if (product == null)
+        {
+            await Send.NotFoundAsync(ct);
+            return;
+        }
+        
+        await productsRepository.DeleteAsync(product, ct);
+
+        await Send.OkAsync(ct);
+    }
+
+}
