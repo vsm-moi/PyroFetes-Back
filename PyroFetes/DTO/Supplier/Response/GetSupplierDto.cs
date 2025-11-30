8a04adeec325654de4eed2a00249988782a63190
sanchezvem <mathys.sanchezvende@gmail.com>
2025-11-30 15:35:08 +0100

    updated GetSupplierDto.cs

--- PyroFetes/DTO/Supplier/Response/GetSupplierDto.cs
+++ PyroFetes/DTO/Supplier/Response/GetSupplierDto.cs
@@ -1,3 +1,4 @@
+using PyroFetes.DTO.Price.Response;
 using PyroFetes.DTO.Product.Response;
 
 namespace PyroFetes.DTO.Supplier.Response;
@@ -13,4 +14,5 @@ public class GetSupplierDto
     public string? City { get; set; }
     public int DeliveryDelay { get; set; }
     public List<GetProductDto>? Products { get; set; }
+    public List<GetPriceDto>? Prices { get; set; }
 }
