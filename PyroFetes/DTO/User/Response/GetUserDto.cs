fb97729c719ffd69fee51078bd4dc2cd1ebae807
sanchezvem <mathys.sanchezvende@gmail.com>
2026-05-28 15:41:07 +0100

    Changed values of userDto

--- PyroFetes/DTO/User/Response/GetUserDto.cs
+++ PyroFetes/DTO/User/Response/GetUserDto.cs
@@ -4,8 +4,6 @@ public class GetUserDto
 {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public string? Password { get; set; }
-    public string? Salt { get; set; }
     public string? Fonction { get; set; }
     public string? Email { get; set; }
 }
